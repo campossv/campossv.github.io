6d92d7cc80ef4eb3592e0206c578d70fbd09b828
Vladimir Campos <167268543+campossv@users.noreply.github.com>
2025-03-04 18:25:57 -0600

    nueva pagina

--- HTML/script.js
+++ HTML/script.js
@@ -1,26 +1,24 @@
-function copyCode(blockId) {
-  const codeBlock = document.getElementById(blockId);
-  const confirmation = document.getElementById(
-    `copyConfirmation${blockId.slice(-1)}`
-  );
-
-  // Crear un elemento de texto temporal
-  const textArea = document.createElement("textarea");
-  textArea.value = codeBlock.textContent.trim();
-
-  // Añadir el elemento al DOM
-  document.body.appendChild(textArea);
-
-  // Seleccionar y copiar el texto
-  textArea.select();
-  document.execCommand("copy");
-
-  // Eliminar el elemento temporal
-  document.body.removeChild(textArea);
-
-  // Mostrar mensaje de confirmación
-  confirmation.classList.add("show");
-  setTimeout(() => {
-    confirmation.classList.remove("show");
-  }, 2000);
+function copyCode(button) {
+    const codeBlock = button.parentElement.querySelector('code');
+    const confirmation = button.parentElement.querySelector('.copy-confirmation');
+    
+    // Crear un elemento de texto temporal
+    const textArea = document.createElement("textarea");
+    textArea.value = codeBlock.textContent.trim();
+    
+    // Añadir el elemento al DOM
+    document.body.appendChild(textArea);
+    
+    // Seleccionar y copiar el texto
+    textArea.select();
+    document.execCommand("copy");
+    
+    // Eliminar el elemento temporal
+    document.body.removeChild(textArea);
+    
+    // Mostrar mensaje de confirmación
+    confirmation.classList.add("show");
+    setTimeout(() => {
+        confirmation.classList.remove("show");
+    }, 2000);
 }
